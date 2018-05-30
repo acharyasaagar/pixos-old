32dbe22da99898483de5dda31042fb079213f337
Sagar Acharya <acharyasaagar@gmail.com>
2018-05-30 22:52:45 +0300

    Update Search.vue

--- src/components/navbar/Search.vue
+++ src/components/navbar/Search.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="form-inline my-2 my-lg-0" v-on:submit="getMovies(query)">
+  <form class="form-inline my-2 my-lg-0" v-on:submit.prevent="getMovies(query)">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="query">
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Search</button>
   </form>
